fffe43e728143f7b1ecabe389bce5c304ddfc5b6
김동준 <ramon@ramonworks.com>
2024-10-28 15:04:09 +0900

    Fix:Crawling error fix

--- src/app/api/exchangeRate/route.ts
+++ src/app/api/exchangeRate/route.ts
@@ -11,10 +11,7 @@ export async function GET(req: NextRequest) {
       headless: true,
       args: [
         '--no-sandbox',
-        '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',
-        '--disable-accelerated-2d-canvas',
-        '--disable-gpu',
       ],
       // AWS Lambda 환경을 위한 추가 설정
       executablePath: process.env.PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH || undefined
